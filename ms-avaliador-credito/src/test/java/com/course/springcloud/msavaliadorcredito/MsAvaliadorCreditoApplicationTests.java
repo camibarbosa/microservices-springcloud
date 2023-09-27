ba866a9a088281f79cc35af0f2c9bfe54fe1a0bd
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-09-27 13:35:53 -0300

    feat: Implementing Avaliador de Credido ms

---
+++ ms-avaliador-credito/src/test/java/com/course/springcloud/msavaliadorcredito/MsAvaliadorCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.springcloud.msavaliadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAvaliadorCreditoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

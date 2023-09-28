5f7c6ceae64767ec4d52136613d09eb407e5733c
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-09-28 13:49:56 -0300

    feat: added infra clients

--- ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/infra/clients/CartoesResourceClient.java
+++ ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/infra/clients/CartoesResourceClient.java
@@ -1,2 +1,16 @@
-package com.course.springcloud.msavaliadorcredito.infra.clients;public interface CartoesResourceClient {
+package com.course.springcloud.msavaliadorcredito.infra.clients;
+
+import com.course.springcloud.msavaliadorcredito.domain.model.CartaoCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartoesResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CartaoCliente>> getCartoesByCliente(@RequestParam("cpf") String cpf);
 }

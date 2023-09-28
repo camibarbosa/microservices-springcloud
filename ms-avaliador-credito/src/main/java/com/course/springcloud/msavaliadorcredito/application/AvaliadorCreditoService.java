d822ce03c35f9caf8881f6e64c02d3e689c94c31
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-09-28 13:42:51 -0300

    feat: added credito controller

--- ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,2 +1,31 @@
-package com.course.springcloud.msavaliadorcredito.application;public class AvaliadorCreditoService {
+package com.course.springcloud.msavaliadorcredito.application;
+
+import com.course.springcloud.msavaliadorcredito.domain.model.CartaoCliente;
+import com.course.springcloud.msavaliadorcredito.domain.model.DadosCliente;
+import com.course.springcloud.msavaliadorcredito.domain.model.SituacaoCliente;
+import com.course.springcloud.msavaliadorcredito.infra.clients.CartoesResourceClient;
+import com.course.springcloud.msavaliadorcredito.infra.clients.ClienteResourceClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AvaliadorCreditoService {
+
+    private  final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesCliente;
+    public SituacaoCliente obterSituacaoCliente(String cpf){
+        // obterDadosCliente - MSCLIENTES
+        // obter cartoes do cliente - MsCartoes
+        ResponseEntity<DadosCliente> dadosClienteResponseEntity = clientesClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesCliente.getCartoesByCliente(cpf);
+        return SituacaoCliente
+                .builder()
+                .cliente(dadosClienteResponseEntity.getBody())
+                .cartoes(cartoesResponse.getBody())
+                .build();
+    }
 }

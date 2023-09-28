c6d447c75835dda1eedf2f026136d2652e179c47
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-09-28 13:39:23 -0300

    feat: added feignClient annotation

--- ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/MsAvaliadorCreditoApplication.java
+++ ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/MsAvaliadorCreditoApplication.java
@@ -3,9 +3,11 @@ package com.course.springcloud.msavaliadorcredito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class MsAvaliadorCreditoApplication {
 
     public static void main(String[] args) {

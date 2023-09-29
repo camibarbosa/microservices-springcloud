bf14797a5527b82ae9d5691acf69b538a6398738
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-09-28 22:46:37 -0300

    update

---
+++ ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/domain/model/CartaoAprovado.java
@@ -0,0 +1,12 @@
+package com.course.springcloud.msavaliadorcredito.domain.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CartaoAprovado {
+    private String cartao;
+    private String bandeira;
+    private BigDecimal limiteAprovado;
+}
---
+++ ms-avaliador-credito/src/main/java/com/course/springcloud/msavaliadorcredito/domain/model/RetornoAvaliacaoCliente.java
@@ -0,0 +1,12 @@
+package com.course.springcloud.msavaliadorcredito.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class RetornoAvaliacaoCliente {
+    private List<CartaoAprovado> cartoes;
+}

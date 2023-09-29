8af2b072e6814a0ea6b5f4ec40bea31e14e2d064
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-09-28 22:11:06 -0300

    feat: Update CartaoSaveRequest.java

--- ms-cartoes/src/main/java/com/course/springcloud/mscartoes/application/dto/CartaoSaveRequest.java
+++ ms-cartoes/src/main/java/com/course/springcloud/mscartoes/application/dto/CartaoSaveRequest.java
@@ -1,5 +1,6 @@
 package com.course.springcloud.mscartoes.application.dto;
 
+import com.course.springcloud.mscartoes.domain.BandeiraCartao;
 import com.course.springcloud.mscartoes.domain.Cartao;
 import lombok.Data;
 
@@ -8,11 +9,11 @@ import java.math.BigDecimal;
 @Data
 public class CartaoSaveRequest {
     private String nome;
-    private String bandeira;
+    private BandeiraCartao bandeira;
     private BigDecimal renda;
     private BigDecimal limite;
 
     public Cartao toModel(){
-        return new Cartao(nome, bandeira, renda, limite);
+        return new Cartao(nome, bandeira, renda, limite );
     }
 }

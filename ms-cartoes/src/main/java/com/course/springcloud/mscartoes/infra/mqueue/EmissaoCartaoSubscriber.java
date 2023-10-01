37db29b4f34bf4810f04e01d8de5093c77ab3960
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-10-01 14:54:44 -0300

    feat: added @Slf4j annotation and log

--- ms-cartoes/src/main/java/com/course/springcloud/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ ms-cartoes/src/main/java/com/course/springcloud/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -7,13 +7,16 @@ import com.course.springcloud.mscartoes.infra.repository.ClienteCartaoRepository
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
+
     private final CartaoRepository cartaoRepository;
     private final ClienteCartaoRepository clienteCartaoRepository;
 
@@ -21,17 +24,19 @@ public class EmissaoCartaoSubscriber {
     public void receberSolicitacaoEmissao(@Payload String payload){
         try {
             var mapper = new ObjectMapper();
+
             DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
             Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+
             ClienteCartao clienteCartao = new ClienteCartao();
             clienteCartao.setCartao(cartao);
             clienteCartao.setCpf(dados.getCpf());
             clienteCartao.setLimite(dados.getLimiteLiberado());
 
             clienteCartaoRepository.save(clienteCartao);
-        } catch (JsonProcessingException e){
-            e.printStackTrace();
-        }
 
+        }catch (Exception e){
+            log.error("Erro ao receber solicitacao de emissao de cartao: {} ", e.getMessage());
+        }
     }
 }
